6d247a2683cb88a30ff3f31f836a2d11143a1763
Daniel Morais Borges <daniel.morais@bumlai.com.br>
2020-12-02 11:39:35 -0400

    Finalizado Front-end consumindo API github

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,69 +1,66 @@
-import React from 'react';
-import { FiChevronRight } from 'react-icons/fi';
+import React, { useState, FormEvent } from "react";
+import { FiChevronRight } from "react-icons/fi";
+import api from "../../services/api";
 
-import logoImg from '../../assets/logo.svg';
+import logoImg from "../../assets/logo.svg";
 
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories } from "./styles";
 
-const Dashboard: React.FC = () =>{
-     return (
-     <>
-      <img src={logoImg} alt="Github Explorer"/>
+interface Repository {
+     full_name: string; 
+     description: string;
+     owner: {
+          login: string;
+          avatar_url: string;
+     };
+}
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
       <Title> Explore repositórios no Github </Title>
 
-      <Form>
-           <input placeholder="Digite o nome do repositorio" />
-           <button> Pesquisar </button>
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositorio"
+        />
+        <button> Pesquisar </button>
       </Form>
 
       <Repositories>
-          <a href="teste">
-               <img
-                 src="https://avatars0.githubusercontent.com/u/54160304?s=400&u=f4b20f68c36b5e41398b81f415babd507f70bcae&v=4"
-                 alt="Daniel Morais" 
-               />          
-          <div>
-               <strong> github.com/danielmoraiscgr </strong>
-               <p> Desenvolvendo os primeiros projetos em React </p>
-          </div>
-          <FiChevronRight size={20} />
-          </a>
-          <a href="teste">
-               <img
-                 src="https://avatars0.githubusercontent.com/u/54160304?s=400&u=f4b20f68c36b5e41398b81f415babd507f70bcae&v=4"
-                 alt="Daniel Morais" 
-               />          
-          <div>
-               <strong> github.com/danielmoraiscgr </strong>
-               <p> Desenvolvendo os primeiros projetos em React </p>
-          </div>
-          <FiChevronRight size={20} />
-          </a>
-          <a href="teste">
-               <img
-                 src="https://avatars0.githubusercontent.com/u/54160304?s=400&u=f4b20f68c36b5e41398b81f415babd507f70bcae&v=4"
-                 alt="Daniel Morais" 
-               />          
-          <div>
-               <strong> github.com/danielmoraiscgr </strong>
-               <p> Desenvolvendo os primeiros projetos em React </p>
-          </div>
-          <FiChevronRight size={20} />
-          </a>
-          <a href="teste">
-               <img
-                 src="https://avatars0.githubusercontent.com/u/54160304?s=400&u=f4b20f68c36b5e41398b81f415babd507f70bcae&v=4"
-                 alt="Daniel Morais" 
-               />          
-          <div>
-               <strong> github.com/danielmoraiscgr </strong>
-               <p> Desenvolvendo os primeiros projetos em React </p>
-          </div>
-          <FiChevronRight size={20} />
-          </a>
+       {repositories.map(repository =>(
+             <a key={repository.full_name} href="teste">
+             <img
+               src={repository.owner.avatar_url}
+               alt={repository.owner.login}
+             />
+             <div>
+               <strong> {repository.full_name} </strong>
+               <p> {repository.description} </p>
+             </div>
+             <FiChevronRight size={20} />
+           </a>
+       ))}
       </Repositories>
-     </>
-     );
+    </>
+  );
 };
 
-export default Dashboard; 
+export default Dashboard;
